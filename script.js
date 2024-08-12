17cc623f4408e7008e6740a3ccc738618c2a2999
msam5345 <sameerarif133@gmail.com>
2024-08-12 20:54:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+// alert("sameer kaha ho")
+// let a=60;
+// a=65
+// console.log(60) 
+// prompt("danish k ghr")
+// console.log("okay")
+
+
+let a = 70;
+a = 90/50
+a="bilal"
+console.log(a)
+
+
+let b=20;
+b = 10+20*5565
+console.log(b)
+
+console.log(10+25) 
+
+
+console.log(20+"24")
+console.log(20-"24")
+console.log(20*"24")
+console.log("java"+2+"number")
+console.log(20/"24")
+
+let txt1 = "hello"
+let txt2 = "SAM"
+let txt3 = " ";
+console.log(txt1 + txt3 + txt2)
+
+let str1 = "10";
+let str2 = "20";
+let str3 = "30";
+console.log(str1 + str3 - str2)
+
+console.log(10**2 + 2**3)
+
+console.log(80-(3+55))
+
+let lastName;
+let firstName;
+let dateOfBirth;
+let date_of_birth;
+let DateOfBirth;
+
+let e = 20;
+let f = 30;
+console.log(a)
+console.log(b)
+
+let aa = prompt ("enter");
+let ab = prompt ("num");
+console.log(aa+ab)
+console.log(aa-ab)
+console.log(aa*ab)
+console.log(aa/ab)
+
+
+
+// let c = prompt("enter your name")
+// console.log(c)
+
+
+// let d = document.getElementById('button');
+
+// d.addEventListener('click',(e)=>{
+//     e.preventDefault();
+
+//     console.log(document.getElementById('name').value);
+// })
+
+
+
+// console.log(d)

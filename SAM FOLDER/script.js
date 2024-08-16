03071a3a4e880081f1339f727bace438f28a48a7
msam5345 <sameerarif133@gmail.com>
2024-08-16 20:55:37 +0500

    Add files via upload

---
+++ SAM FOLDER/script.js
@@ -0,0 +1,143 @@
+
+
+// let num1 = prompt("enter")
+// let num2 = prompt("number")
+
+
+// document.write((+num1) + (+num2) + "<br>")
+// document.write((+num1) - (+num2) + "<br>")
+// document.write((+num1) * (+num2) + "<br>")
+// document.write((+num1) / (+num2) + "<br>")
+
+
+// console.log(parseFloat(num1))
+// console.log(parseInt(num1))
+// console.log((+num1) + (+num2))
+// console.log(+num1)
+// console.log(num2)
+// console.log((+num1) + (+num2))
+
+// let age;
+
+// // age = [25,26,27,28,
+// //     29,30,31,32]
+// age = ["zahid","zain", "usama", "saif", "Ahad" ]
+//  let  persons = ["anas", "riaz",25,"karachi", "female","pakistan","bsse","labour"   
+//  ]
+// // let isalive = false
+// // console.log(isalive)
+// console.log(persons[4])
+
+// console.log(age);
+// document.write(age)
+
+// console.log(persons);
+
+
+// let person = {
+//     FirstName:'Aslam',
+//     LastName: 'Riaz',
+//     Age: 64,
+//     City:'Karachi',
+//     Gender:'Male',
+//     Country:'Pakistan',
+//     Education:'Bsse',
+//     Job: 'labour Manager',
+//     HeIsAlive: true
+// } 
+
+// console.log(person)
+// console.table(person)
+
+// console.error(person)
+// document.write(person)
+
+let persons = [
+    {
+    FirstName:'Aslam',
+    LastName: 'Riaz',
+    Age: 64,
+    City:'Karachi',
+    Gender:'Male',
+    Country:'Pakistan',
+    Education:'Bsse',
+    Job: 'labour Manager',
+    HeIsAlive: true
+},
+{
+    FirstName:'Aslam',
+    LastName: 'Riaz',
+    Age: 64,
+    City:'Karachi',
+    Gender:'Male',
+    Country:'Pakistan',
+    Education:'Bsse',
+    Job: 'labour Manager',
+    HeIsAlive: true
+},
+{
+    FirstName:'Aslam',
+    LastName: 'Riaz',
+    Age: 64,
+    City:'Karachi',
+    Gender:'Male',
+    Country:'Pakistan',
+    Education:'Bsse',
+    Job: 'labour Manager',
+    HeIsAlive: true
+},
+{
+    FirstName:'Aslam',
+    LastName: 'Riaz',
+    Age: 64,
+    City:'Karachi',
+    Gender:'Male',
+    Country:'Pakistan',
+    Education:'Bsse',
+    Job: 'labour Manager',
+    HeIsAlive: true
+},
+{
+    FirstName:'Aslam',
+    LastName: 'Riaz',
+    Age: 64,
+    City:'Karachi',
+    Gender:'Male',
+    Country:'Pakistan',
+    Education:'Bsse',
+    Job: 'labour Manager',
+    HeIsAlive: true
+},
+{
+    FirstName:'Aslam',
+    LastName: 'Riaz',
+    Age: 64,
+    City:'Karachi',
+    Gender:'Male',
+    Country:'Pakistan',
+    Education:'Bsse',
+    Job: 'labour Manager',
+    HeIsAlive: true
+},
+
+
+
+
+] 
+
+
+
+console.log(persons)
+console.table(persons)
+
+
+
+
+for(let i =0; i < persons.length;i++)
+    {
+        console.log(persons[i].FirstName)
+        console.log(persons[i].LastName)
+        console.log(persons[i].gender)
+        console.log(persons[i].Age)
+
+    }

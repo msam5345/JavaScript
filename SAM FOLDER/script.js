b0464ddb050795bb1be5298d6da74abd5f79a633
msam5345 <sameerarif133@gmail.com>
2024-08-23 20:39:11 +0500

    Add files via upload

--- SAM FOLDER/script.js
+++ SAM FOLDER/script.js
@@ -52,92 +52,194 @@
 // console.error(person)
 // document.write(person)
 
-let persons = [
-    {
-    FirstName:'Aslam',
-    LastName: 'Riaz',
-    Age: 64,
-    City:'Karachi',
-    Gender:'Male',
-    Country:'Pakistan',
-    Education:'Bsse',
-    Job: 'labour Manager',
-    HeIsAlive: true
-},
-{
-    FirstName:'Aslam',
-    LastName: 'Riaz',
-    Age: 64,
-    City:'Karachi',
-    Gender:'Male',
-    Country:'Pakistan',
-    Education:'Bsse',
-    Job: 'labour Manager',
-    HeIsAlive: true
-},
-{
-    FirstName:'Aslam',
-    LastName: 'Riaz',
-    Age: 64,
-    City:'Karachi',
-    Gender:'Male',
-    Country:'Pakistan',
-    Education:'Bsse',
-    Job: 'labour Manager',
-    HeIsAlive: true
-},
-{
-    FirstName:'Aslam',
-    LastName: 'Riaz',
-    Age: 64,
-    City:'Karachi',
-    Gender:'Male',
-    Country:'Pakistan',
-    Education:'Bsse',
-    Job: 'labour Manager',
-    HeIsAlive: true
-},
-{
-    FirstName:'Aslam',
-    LastName: 'Riaz',
-    Age: 64,
-    City:'Karachi',
-    Gender:'Male',
-    Country:'Pakistan',
-    Education:'Bsse',
-    Job: 'labour Manager',
-    HeIsAlive: true
-},
-{
-    FirstName:'Aslam',
-    LastName: 'Riaz',
-    Age: 64,
-    City:'Karachi',
-    Gender:'Male',
-    Country:'Pakistan',
-    Education:'Bsse',
-    Job: 'labour Manager',
-    HeIsAlive: true
-},
+// let persons = [
+//     {
+//     FirstName:'Aslam',
+//     LastName: 'Riaz',
+//     Age: 64,
+//     City:'Karachi',
+//     Gender:'Male',
+//     Country:'Pakistan',
+//     Education:'Bsse',
+//     Job: 'labour Manager',
+//     HeIsAlive: true
+// },
+// {
+//     FirstName:'Aslam',
+//     LastName: 'Riaz',
+//     Age: 64,
+//     City:'Karachi',
+//     Gender:'Male',
+//     Country:'Pakistan',
+//     Education:'Bsse',
+//     Job: 'labour Manager',
+//     HeIsAlive: true
+// },
+// {
+//     FirstName:'Aslam',
+//     LastName: 'Riaz',
+//     Age: 64,
+//     City:'Karachi',
+//     Gender:'Male',
+//     Country:'Pakistan',
+//     Education:'Bsse',
+//     Job: 'labour Manager',
+//     HeIsAlive: true
+// },
+// {
+//     FirstName:'Aslam',
+//     LastName: 'Riaz',
+//     Age: 64,
+//     City:'Karachi',
+//     Gender:'Male',
+//     Country:'Pakistan',
+//     Education:'Bsse',
+//     Job: 'labour Manager',
+//     HeIsAlive: true
+// },
+// {
+//     FirstName:'Aslam',
+//     LastName: 'Riaz',
+//     Age: 64,
+//     City:'Karachi',
+//     Gender:'Male',
+//     Country:'Pakistan',
+//     Education:'Bsse',
+//     Job: 'labour Manager',
+//     HeIsAlive: true
+// },
+// {
+//     FirstName:'Aslam',
+//     LastName: 'Riaz',
+//     Age: 64,
+//     City:'Karachi',
+//     Gender:'Male',
+//     Country:'Pakistan',
+//     Education:'Bsse',
+//     Job: 'labour Manager',
+//     HeIsAlive: true
+// },
 
 
 
 
-] 
+// ] 
 
 
 
-console.log(persons)
-console.table(persons)
+// console.log(persons)
+// console.table(persons)
 
 
 
 
-for(let i =0; i < persons.length;i++)
-    {
-        console.log(persons[i].FirstName)
-        console.log(persons[i].LastName)
-        console.log(persons[i].gender)
-        console.log(persons[i].Age)
+// for(let i =0; i < persons.length;i++)
+//     {
+//         console.log(persons[i].FirstName)
+//         console.log(persons[i].LastName)
+//         console.log(persons[i].gender)
+//         console.log(persons[i].Age)
 
-    }
+//     }
+
+// let names = ['sameer', 'zain','ahad','saad', 'uzair']
+// console.log(names)
+// document.write(names[0] + "<br>")
+// document.write(names[1] + "<br>")
+// document.write(names[2] + "<br>")
+// document.write(names[3] + "<br>")
+// document.write(names[4] + "<br>")
+
+// for(let index = 0; index < 4;index++)
+//     {
+//         document.write(names[index] + "<br>")
+//     }
+
+// for
+// while
+// do while
+
+// console.log("length",names.length)
+
+// for(let index = 0; index <names.length; index++)
+//     {
+//         document.write(names[index] + "<br>")
+//     }
+
+
+    // Some more useful Operators
+
+    // INCREMENT OPERATOR
+    // a++         ------------------------> postfix increment
+    // ++a        ------------------------->prefix increment
+
+    // DECREMENT
+
+    // a--   --------------------------->postfix increment
+    // --a  --------------------------->prefix increment
+
+    // ----------------------------------------------
+    // a+=2
+    // a-=2
+    // a*=2
+    // a/=2
+
+
+    // for(let index = 0; index < names.length; index++)
+    //     {
+    //         document.write(names[index] + "<br>")
+    //     }
+    // for(let i = 0; i < names.length; i++)
+    //     {
+    //         document.write(names[i] + "<br>")
+    //     }
+
+    // let names = ['sameer', 'zain','ahad','saad', 'uzair']
+    
+    // for(let i = 0; i < names.length;i++)
+    // {
+    //     console.log(names[i])
+    // }
+    // console.log(names)
+
+    // for(let i=1; i <= 10; i++)
+    //     {
+    //         console.log(i)
+    //     }
+
+        
+    // for(let i=1;i <= 10; i++)
+    //     {
+    //         console.log(`2 X ${i}=${i*2}`)
+    //     }
+
+
+// let i =;
+
+
+// while(i < names.length)
+//     {
+//         console.log(names[i]);
+//         i++
+
+//     }
+
+// for(;i<names.length;)
+//     {
+//         console.log(names[i]);
+//         i
+//     }
+
+// // do{
+//     console.log(names[i]);
+
+// }
+// while(i > names.length);
+
+// let userinpt;  
+// do
+// {
+//   userinpt = prompt('Enter your value');
+// console.log(userinpt)
+// }
+// while(userinpt == '')
